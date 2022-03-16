7e1cc94ea48abe5a3401cb233fd305da2553dabd
Tran Minh Hieu <tranhieu23.code@outlook.com>
2022-03-16 18:41:22 +0800

    implement database initialization of image service

---
+++ migrations/20220315150438_initialization.ts
@@ -0,0 +1,310 @@
+import { Knex } from "knex";
+
+const TabNameImageServiceImageType = "image_service_image_type_tab";
+const TabNameImageServiceRegionLabel = "image_service_region_label_tab";
+const TabNameImageServiceImageTagGroup = "image_service_image_tag_group_tab";
+const TabNameImageServiceImageTag = "image_service_image_tag_tab";
+const TabNameImageServiceImageTypeHasImageTagGroup =
+    "image_service_image_type_has_image_tag_group_tab";
+const TabNameImageServiceImage = "image_service_image_tab";
+const TabNameImageServiceImageHasTag = "image_service_image_has_tag_tab";
+const TabNameImageServiceRegion = "image_service_region_tab";
+const TabNameImageServiceRegionOperationLog =
+    "image_service_region_operation_log";
+const TabNameImageServiceRegionOperationLogDrawMetadata =
+    "image_service_region_operation_log_draw_metadata";
+const TabNameImageServiceRegionOperationLogLabelMetadata =
+    "image_service_region_operation_log_label_metadata";
+const TabNameImageServiceRegionSnapshot = "image_service_region_snapshot_tab";
+
+export async function up(knex: Knex): Promise<void> {
+    if (!(await knex.schema.hasTable(TabNameImageServiceImageType))) {
+        await knex.schema.createTable(TabNameImageServiceImageType, (tab) => {
+            tab.increments("id", { primaryKey: true });
+            tab.string("display_name", 256).notNullable();
+            tab.boolean("has_predictive_model").notNullable();
+        });
+    }
+
+    if (!(await knex.schema.hasTable(TabNameImageServiceRegionLabel))) {
+        await knex.schema.createTable(TabNameImageServiceRegionLabel, (tab) => {
+            tab.increments("id", { primaryKey: true });
+            tab.integer("of_image_type_id").notNullable();
+            tab.string("display_name", 256).notNullable();
+            tab.string("color", 7).notNullable();
+
+            tab.foreign("of_image_type_id")
+                .references("id")
+                .inTable(TabNameImageServiceImageType)
+                .onDelete("CASCADE");
+
+            tab.index(
+                ["of_image_type_id"],
+                "image_service_region_label_of_image_type_id_idx"
+            );
+        });
+    }
+
+    if (!(await knex.schema.hasTable(TabNameImageServiceImageTagGroup))) {
+        await knex.schema.createTable(
+            TabNameImageServiceImageTagGroup,
+            (tab) => {
+                tab.increments("id", { primaryKey: true });
+                tab.string("display_name", 256).notNullable();
+                tab.boolean("is_single_value").notNullable();
+            }
+        );
+    }
+
+    if (!(await knex.schema.hasTable(TabNameImageServiceImageTag))) {
+        await knex.schema.createTable(TabNameImageServiceImageTag, (tab) => {
+            tab.increments("id", { primaryKey: true });
+            tab.integer("of_image_tag_group_id").notNullable();
+            tab.string("display_name", 256).notNullable();
+
+            tab.foreign("of_image_tag_group_id")
+                .references("id")
+                .inTable(TabNameImageServiceImageTagGroup)
+                .onDelete("CASCADE");
+
+            tab.index(
+                ["of_image_tag_group_id"],
+                "image_service_region_label_of_image_tag_group_id_idx"
+            );
+        });
+    }
+
+    if (
+        !(await knex.schema.hasTable(
+            TabNameImageServiceImageTypeHasImageTagGroup
+        ))
+    ) {
+        await knex.schema.createTable(
+            TabNameImageServiceImageTypeHasImageTagGroup,
+            (tab) => {
+                tab.integer("image_type_id").notNullable();
+                tab.integer("image_tag_group_id").notNullable();
+
+                tab.foreign("image_type_id")
+                    .references("id")
+                    .inTable(TabNameImageServiceImageType)
+                    .onDelete("CASCADE");
+                tab.foreign("image_tag_group_id")
+                    .references("id")
+                    .inTable(TabNameImageServiceImageTagGroup)
+                    .onDelete("CASCADE");
+
+                tab.unique(["image_type_id", "image_tag_group_id"], {
+                    indexName:
+                        "image_service_image_type_has_image_tag_group_image_type_id_image_tag_group_id_idx",
+                });
+
+                tab.index(
+                    ["image_tag_group_id"],
+                    "image_service_image_type_has_image_tag_group_image_tag_group_id_idx"
+                );
+            }
+        );
+    }
+
+    if (!(await knex.schema.hasTable(TabNameImageServiceImage))) {
+        await knex.schema.createTable(TabNameImageServiceImage, (tab) => {
+            tab.increments("id", { primaryKey: true });
+            tab.integer("uploaded_by_user_id").notNullable().defaultTo(0);
+            tab.bigInteger("upload_time").notNullable().defaultTo(0);
+            tab.integer("published_by_user_id").notNullable().defaultTo(0);
+            tab.bigInteger("publish_time").notNullable().defaultTo(0);
+            tab.integer("verified_by_user_id").notNullable().defaultTo(0);
+            tab.bigInteger("verify_time").notNullable().defaultTo(0);
+            tab.string("original_file_name", 256).notNullable().defaultTo("");
+            tab.string("image_url", 256).notNullable();
+            tab.string("thumbnail_url", 256).notNullable();
+            tab.string("description", 256).notNullable().defaultTo("");
+            tab.integer("image_type_id").defaultTo(null);
+            tab.smallint("status").notNullable().defaultTo(0);
+
+            tab.foreign("image_type_id")
+                .references("id")
+                .inTable(TabNameImageServiceImageType)
+                .onDelete("SET NULL");
+
+            tab.index(["upload_time"], "image_service_image_upload_time_idx");
+            tab.index(["publish_time"], "image_service_image_publish_time_idx");
+            tab.index(["verify_time"], "image_service_image_verify_time_idx");
+        });
+    }
+
+    if (!(await knex.schema.hasTable(TabNameImageServiceImageHasTag))) {
+        await knex.schema.createTable(TabNameImageServiceImageHasTag, (tab) => {
+            tab.integer("image_id").notNullable();
+            tab.integer("image_tag_id").notNullable();
+
+            tab.foreign("image_id")
+                .references("id")
+                .inTable(TabNameImageServiceImage)
+                .onDelete("CASCADE");
+            tab.foreign("image_tag_id")
+                .references("id")
+                .inTable(TabNameImageServiceImageTag)
+                .onDelete("CASCADE");
+
+            tab.unique(["image_id", "image_tag_id"], {
+                indexName:
+                    "image_service_image_has_image_tag_image_id_image_tag_id_idx",
+            });
+
+            tab.index(
+                ["image_tag_id"],
+                "image_service_image_has_tag_image_tag_id_idx"
+            );
+        });
+    }
+
+    if (!(await knex.schema.hasTable(TabNameImageServiceRegion))) {
+        await knex.schema.createTable(TabNameImageServiceRegion, (tab) => {
+            tab.increments("id", { primaryKey: true });
+            tab.integer("of_image_id").notNullable();
+            tab.integer("drawn_by_user_id").notNullable().defaultTo(0);
+            tab.integer("labeled_by_user_id").notNullable().defaultTo(0);
+            tab.binary("border").notNullable();
+            tab.binary("holes").notNullable();
+            tab.integer("label_id").defaultTo(null);
+
+            tab.foreign("of_image_id")
+                .references("id")
+                .inTable(TabNameImageServiceImage);
+            tab.foreign("label_id")
+                .references("id")
+                .inTable(TabNameImageServiceRegionLabel)
+                .onDelete("SET NULL");
+
+            tab.index(["of_image_id"], "image_service_region_of_image_id_idx");
+            tab.index(["label_id"], "image_service_region_label_id_idx");
+        });
+    }
+
+    if (!(await knex.schema.hasTable(TabNameImageServiceRegionOperationLog))) {
+        await knex.schema.createTable(
+            TabNameImageServiceRegionOperationLog,
+            (tab) => {
+                tab.increments("id", { primaryKey: true });
+                tab.integer("of_region_id").notNullable();
+                tab.integer("by_user_id").notNullable().defaultTo(0);
+                tab.bigInteger("operation_time").notNullable();
+
+                tab.foreign("of_region_id")
+                    .references("id")
+                    .inTable(TabNameImageServiceRegion);
+
+                tab.index(
+                    ["of_region_id"],
+                    "image_service_region_operation_log_of_region_id_idx"
+                );
+            }
+        );
+    }
+
+    if (
+        !(await knex.schema.hasTable(
+            TabNameImageServiceRegionOperationLogDrawMetadata
+        ))
+    ) {
+        await knex.schema.createTable(
+            TabNameImageServiceRegionOperationLogDrawMetadata,
+            (tab) => {
+                tab.integer("of_log_id");
+                tab.binary("old_border").notNullable();
+                tab.binary("old_holes").notNullable();
+                tab.binary("new_border").notNullable();
+                tab.binary("new_holes").notNullable();
+
+                tab.primary(["of_log_id"]);
+
+                tab.foreign("of_log_id")
+                    .references("id")
+                    .inTable(TabNameImageServiceRegion)
+                    .onDelete("CASCADE");
+            }
+        );
+    }
+
+    if (
+        !(await knex.schema.hasTable(
+            TabNameImageServiceRegionOperationLogLabelMetadata
+        ))
+    ) {
+        await knex.schema.createTable(
+            TabNameImageServiceRegionOperationLogLabelMetadata,
+            (tab) => {
+                tab.integer("of_log_id");
+                tab.integer("old_label_id");
+                tab.integer("new_label_id");
+
+                tab.primary(["of_log_id"]);
+
+                tab.foreign("of_log_id")
+                    .references("id")
+                    .inTable(TabNameImageServiceRegion)
+                    .onDelete("CASCADE");
+                tab.foreign("old_label_id")
+                    .references("id")
+                    .inTable(TabNameImageServiceRegionLabel)
+                    .onDelete("SET NULL");
+                tab.foreign("new_label_id")
+                    .references("id")
+                    .inTable(TabNameImageServiceRegionLabel)
+                    .onDelete("SET NULL");
+            }
+        );
+    }
+
+    if (!(await knex.schema.hasTable(TabNameImageServiceRegionSnapshot))) {
+        await knex.schema.createTable(
+            TabNameImageServiceRegionSnapshot,
+            (tab) => {
+                tab.increments("id", { primaryKey: true });
+                tab.integer("of_image_id").notNullable();
+                tab.smallint("at_status").notNullable();
+                tab.integer("drawn_by_user_id").notNullable().defaultTo(0);
+                tab.integer("labeled_by_user_id").notNullable().defaultTo(0);
+                tab.binary("border").notNullable();
+                tab.binary("holes").notNullable();
+                tab.integer("label_id").defaultTo(null);
+
+                tab.foreign("of_image_id")
+                    .references("id")
+                    .inTable(TabNameImageServiceImage);
+                tab.foreign("label_id")
+                    .references("id")
+                    .inTable(TabNameImageServiceRegionLabel)
+                    .onDelete("SET NULL");
+
+                tab.index(
+                    ["of_image_id"],
+                    "image_service_region_snapshot_of_image_id_idx"
+                );
+            }
+        );
+    }
+}
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTableIfExists(TabNameImageServiceImageType);
+    await knex.schema.dropTableIfExists(TabNameImageServiceRegionLabel);
+    await knex.schema.dropTableIfExists(TabNameImageServiceImageTagGroup);
+    await knex.schema.dropTableIfExists(TabNameImageServiceImageTag);
+    await knex.schema.dropTableIfExists(
+        TabNameImageServiceImageTypeHasImageTagGroup
+    );
+    await knex.schema.dropTableIfExists(TabNameImageServiceImage);
+    await knex.schema.dropTableIfExists(TabNameImageServiceImageHasTag);
+    await knex.schema.dropTableIfExists(TabNameImageServiceRegion);
+    await knex.schema.dropTableIfExists(TabNameImageServiceRegionOperationLog);
+    await knex.schema.dropTableIfExists(
+        TabNameImageServiceRegionOperationLogDrawMetadata
+    );
+    await knex.schema.dropTableIfExists(
+        TabNameImageServiceRegionOperationLogLabelMetadata
+    );
+    await knex.schema.dropTableIfExists(TabNameImageServiceRegionSnapshot);
+}
