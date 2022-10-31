0f884d282c4612e3a33fca00f147cc0dd65180de
Tran Minh Hieu <minhhieu.tran@shopee.com>
2022-10-31 20:45:04 +0800

    remove file deletion logics

--- src/module/image/image_processor.ts
+++ src/module/image/image_processor.ts
@@ -1,33 +1,15 @@
 import { injected, token } from "brandi";
-import { writeFile, unlink } from "fs";
 import sharp from "sharp";
 
 export interface ImageProcessor {
-    resizeImage(
-        imageData: Buffer,
-        minWidth: number,
-        minHeigh: number
-    ): Promise<Buffer>;
-    saveImageFile(filename: string, imageData: Buffer): Promise<void>;
-    deleteImageFile(filename: string): Promise<void>;
+    resizeImage(imageData: Buffer, minWidth: number, minHeigh: number): Promise<Buffer>;
 }
 
 export class ImageProcessorImpl implements ImageProcessor {
-    public async resizeImage(
-        imageData: Buffer,
-        minWidth: number,
-        minHeigh: number
-    ): Promise<Buffer> {
+    public async resizeImage(imageData: Buffer, minWidth: number, minHeigh: number): Promise<Buffer> {
         let image = sharp(imageData);
         const imageMetadata = await image.metadata();
-        if (
-            this.checkImageNeedResizing(
-                imageMetadata.width || 0,
-                imageMetadata.height || 0,
-                minWidth,
-                minHeigh
-            )
-        ) {
+        if (this.checkImageNeedResizing(imageMetadata.width || 0, imageMetadata.height || 0, minWidth, minHeigh)) {
             image = image.resize(minWidth, minHeigh, { fit: "outside" });
         }
         return image.rotate().toBuffer();
@@ -42,31 +24,6 @@ export class ImageProcessorImpl implements ImageProcessor {
         if (imageWidth < minWidth) return false;
         if (imageHeight < minHeight) return false;
         return true;
-    }
-
-    public async saveImageFile(
-        filename: string,
-        imageData: Buffer
-    ): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            writeFile(filename, imageData, (error) => {
-                if (error) {
-                    return reject(error);
-                }
-                resolve();
-            });
-        });
-    }
-
-    public async deleteImageFile(filename: string): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            unlink(filename, (error) => {
-                if (error) {
-                    return reject(error);
-                }
-                resolve();
-            });
-        });
     }
 }
 
