1d234cf5d2a098353f858c7978a1ab688510f6a9
Tran Minh Hieu <minhhieu.tran@shopee.com>
2022-10-31 20:47:37 +0800

    update ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
         {
             name: "image_service",
             script: "./dist/main.js",
+            args: " --start_grpc_server",
             instances: 8,
             instance_var: "NODE_ID",
         },
