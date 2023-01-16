2460bd25064554f7ef99b8d82f3db9e1d063ecf8
Trần Minh Hiếu <tranhieudev23@Trans-MacBook-Pro.local>
2023-01-16 09:40:13 +0700

    allow creation of self-verification permisssion

--- src/module/user_can_verify_user_image/user_can_verify_user_image_management_operator.ts
+++ src/module/user_can_verify_user_image/user_can_verify_user_image_management_operator.ts
@@ -1,18 +1,12 @@
-import { status } from "@grpc/grpc-js";
 import { injected, token } from "brandi";
-import { Logger } from "winston";
 import {
     UserCanVerifyUserImageDataAccessor,
     USER_CAN_VERIFY_USER_IMAGE_DATA_ACCESSOR_TOKEN,
 } from "../../dataaccess/db";
 import { UserCanVerifyUserImage } from "../../proto/gen/UserCanVerifyUserImage";
-import { ErrorWithStatus, LOGGER_TOKEN } from "../../utils";
 
 export interface UserCanVerifyUserImageManagementOperator {
-    createUserCanVerifyUserImage(
-        userId: number,
-        imageOfUserId: number
-    ): Promise<void>;
+    createUserCanVerifyUserImage(userId: number, imageOfUserId: number): Promise<void>;
     getUserCanVerifyUserImageOfUserId(
         userId: number,
         offset: number,
@@ -21,38 +15,14 @@ export interface UserCanVerifyUserImageManagementOperator {
         totalUserCount: number;
         userList: UserCanVerifyUserImage[];
     }>;
-    deleteUserCanVerifyUserImage(
-        userId: number,
-        imageOfUserId: number
-    ): Promise<void>;
+    deleteUserCanVerifyUserImage(userId: number, imageOfUserId: number): Promise<void>;
 }
 
-export class UserCanVerifyUserImageManagementOperatorImpl
-    implements UserCanVerifyUserImageManagementOperator
-{
-    constructor(
-        private readonly userCanVerifyUserImageDM: UserCanVerifyUserImageDataAccessor,
-        private readonly logger: Logger
-    ) {}
+export class UserCanVerifyUserImageManagementOperatorImpl implements UserCanVerifyUserImageManagementOperator {
+    constructor(private readonly userCanVerifyUserImageDM: UserCanVerifyUserImageDataAccessor) {}
 
-    public async createUserCanVerifyUserImage(
-        userId: number,
-        imageOfUserId: number
-    ): Promise<void> {
-        if (userId === imageOfUserId) {
-            this.logger.error(
-                "trying to add user can verify user image relation for the same user",
-                { userId }
-            );
-            throw new ErrorWithStatus(
-                "trying to add user can verify user image relation for the same user",
-                status.INVALID_ARGUMENT
-            );
-        }
-        await this.userCanVerifyUserImageDM.createUserCanVerifyUserImage(
-            userId,
-            imageOfUserId
-        );
+    public async createUserCanVerifyUserImage(userId: number, imageOfUserId: number): Promise<void> {
+        await this.userCanVerifyUserImageDM.createUserCanVerifyUserImage(userId, imageOfUserId);
     }
 
     public async getUserCanVerifyUserImageOfUserId(
@@ -61,38 +31,21 @@ export class UserCanVerifyUserImageManagementOperatorImpl
         limit: number
     ): Promise<{ totalUserCount: number; userList: UserCanVerifyUserImage[] }> {
         const dmResults = await Promise.all([
-            this.userCanVerifyUserImageDM.getUserCanVerifyUserImageCountOfUserId(
-                userId
-            ),
-            this.userCanVerifyUserImageDM.getUserCanVerifyUserImageListOfUserId(
-                userId,
-                offset,
-                limit
-            ),
+            this.userCanVerifyUserImageDM.getUserCanVerifyUserImageCountOfUserId(userId),
+            this.userCanVerifyUserImageDM.getUserCanVerifyUserImageListOfUserId(userId, offset, limit),
         ]);
         const totalUserCount = dmResults[0];
         const userList = dmResults[1];
         return { totalUserCount, userList };
     }
 
-    public async deleteUserCanVerifyUserImage(
-        userId: number,
-        imageOfUserId: number
-    ): Promise<void> {
-        await this.userCanVerifyUserImageDM.deleteUserCanVerifyUserImage(
-            userId,
-            imageOfUserId
-        );
+    public async deleteUserCanVerifyUserImage(userId: number, imageOfUserId: number): Promise<void> {
+        await this.userCanVerifyUserImageDM.deleteUserCanVerifyUserImage(userId, imageOfUserId);
     }
 }
 
-injected(
-    UserCanVerifyUserImageManagementOperatorImpl,
-    USER_CAN_VERIFY_USER_IMAGE_DATA_ACCESSOR_TOKEN,
-    LOGGER_TOKEN
-);
+injected(UserCanVerifyUserImageManagementOperatorImpl, USER_CAN_VERIFY_USER_IMAGE_DATA_ACCESSOR_TOKEN);
 
-export const USER_CAN_VERIFY_USER_IMAGE_MANAGEMENT_OPERATOR =
-    token<UserCanVerifyUserImageManagementOperator>(
-        "UserCanVerifyUserImageVerifyManagementOperator"
-    );
+export const USER_CAN_VERIFY_USER_IMAGE_MANAGEMENT_OPERATOR = token<UserCanVerifyUserImageManagementOperator>(
+    "UserCanVerifyUserImageVerifyManagementOperator"
+);
